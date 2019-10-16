8318ab97456e7da8ad4d34c54faf682364256971
cehassin <cehassin@gmail.com>
2019-10-16 15:29:00 -0700

    'this' is done

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,62 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding is when you console log whatever you set "this" to be.
+* 2. Implicit Binding is you create an object and in that you have a function (method). what you invoke the object and method, "this" refers to the values you set inside the object, which is left of the dot.
+* 3. New binding is when you take make a new object from a previous object, but the value(s) of "this" is kept in the old object, and a new parameter can be passed in the newer object.
+* 4. Explicit binding is when you have a function and object on the global scope and you combine/invoke them with the use of call, apply, and bind.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function roar(){
+    console.log(this.lionSound);
+  }
+  
+  var lionSound = "roarrrrrrrrr";
+  
+  roar();
 
 // Principle 2
 
-// code example for Implicit Binding
+/ const myFavThings = {
+    appliance: "refridgerator",
+    food: "chicken",
+    combineTheTwo: function(name){
+      console.log(`What are ${name}'s favorite things you ask? ${this.appliance} and ${this.food}'`);
+      console.log(this);
+    }
+  };
+  
+  myFavThings.combineTheTwo("carlos");
 
 // Principle 3
 
-// code example for New Binding
+function favPhrase(saying){
+  this.phrase = saying;
+}
+
+let myfavPhrase = new Phrase("I like Turtles");
+
+console.log(myfavPhrase.phrase);
 
 // Principle 4
 
-// code example for Explicit Binding
+function dog(){
+  console.log(this.bark);
+}
+
+let myDog = {
+  name: 'Tyberious',
+  bark: 'BWERWEK!'
+}
+
+let neighborDog = {
+  name: 'mark',
+  bark: "arararararg!"
+}
+
+dog.call(myDog);
+dog.call(neighborDog);
